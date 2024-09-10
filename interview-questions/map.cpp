6895794dff853cb42b89b11bbec14b4ba451e5fa
Girish Pandit <girschol7@gmail.com>
2024-09-09 22:27:00 -0700

    Initial commit

---
+++ interview-questions/map.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include <string>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    // vector<string> msg = {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    cout<<"Hello World!";
+    return 0;
+}
